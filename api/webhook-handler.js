433af09a17f641aad6d107e5c6aa029e7da993f6
d0ctr <mr.den.vi@gmail.com>
2022-11-17 19:40:18 +0100

    Added payload formatter for github

--- api/webhook-handler.js
+++ api/webhook-handler.js
@@ -6,6 +6,37 @@ const logger = require('../logger').child({ module: 'webhook-handler' });
 /**
  * Handler for Webhook POST from another app
  * @param {expressTypes.Request} request 
+ */
+function formatGithubWebhook(request) {
+    let text = `<b>${request.params.app}</b>\n`;
+
+    let payload = request.body;
+
+    switch(request.get('X-GitHub-Event').toLowerCase()) {
+        case 'pull_request':
+            text += `${payload.action[0].toUpperCase() + payload.action.substr(1)} PR <a href="${payload.pull_request.html_url}">#${payload.number}</a>\n`;
+            text += `<u>${payload.pull_request.tile}</u>\n`;
+            text += `<i>by <a href="${payload.pull_request.user.html_url}">@${payload.pull_request.user.login}</a></i>\n`;
+            break;
+        case 'deplyment_status':
+            text += `Deployment status: <a href="${payload.deployment_status.deployment_url}">${payload.deployment_status.state}</a>\n`;
+            text += `Description: <i>${payload.deployment_status.description}</i>\n`;
+            text += `<i>by <a href="${payload.pull_request.user.html_url}">@${payload.pull_request.user.login}</a></i>\n`;
+        default:
+            return '';
+    }
+
+    return text;
+}
+
+
+const formaters = {
+    'github': formatGithubWebhook
+}
+
+/**
+ * Handler for Webhook POST from another app
+ * @param {expressTypes.Request} request 
  * @param {expressTypes.Response} response 
  */
 function handleWebhook(request, response) {
@@ -15,11 +46,16 @@ function handleWebhook(request, response) {
 
     let message_text = '';
 
-    if (request.params.app) {
-        message_text += `<b>${request.params.app}</b>\n`;
+    if (request.params.app && formaters[request.params.app.toLowerCase()]) {
+        message_text = formaters[request.params.app.toLowerCase()](request);
     }
-    if (request.body) {
-        message_text += `<code>${JSON.stringify(request.body, null, 4)}</code>`;
+    else {
+        if (request.params.app) {
+            message_text += `<b>${request.params.app}</b>\n`;
+        }
+        if (request.body) {
+            message_text += `<code>${JSON.stringify(request.body, null, 4)}</code>`;
+        }
     }
     
     if (message_text === '') return;
