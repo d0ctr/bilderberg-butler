20a1bacabf423868dc4965fb1a17613a11e118e9
d0ctr <mr.den.vi@gmail.com>
2022-11-29 00:06:32 +0100

    Adding dotenv loader to logger file

--- logger.js
+++ logger.js
@@ -1,6 +1,12 @@
 const { createLogger, format, transports } = require('winston');
 const LokiTransport = require('winston-loki');
 
+require('dotenv-vault-core').config();
+
+if (process.env.ENV !== 'prod') {
+    require('dotenv').config();
+}
+
 const ENABLE_LOKI = process.env.ENABLE_LOKI === 'true';
 
 const logger_options = {
