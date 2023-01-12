0f1335144f6f822a874537c1e1dc3a13f89dad10
Danila Vasilev <33842017+d0ctr@users.noreply.github.com>
2023-01-12 19:42:28 +0100

    Deleted unexisting export

--- commands/utils.js
+++ commands/utils.js
@@ -2,5 +2,5 @@ const urlStartRegex = /^(https*:\/\/)*/;
 const russianAlphabetRegex = /[а-яА-Я]+/gm;
 
 module.exports = {
-    getRegex, urlStartRegex, russianAlphabetRegex
+    urlStartRegex, russianAlphabetRegex
 }
