9d63619a35021dd515d002763ed759149245ab76
d0ctr <mr.den.vi@gmail.com>
2023-01-29 02:42:35 +0100

    Mmber name in notification by default

--- telegram/channel-subscriber.js
+++ telegram/channel-subscriber.js
@@ -98,10 +98,11 @@ class DiscordNotification {
         let text = `Канал <a href="${this.getChannelUrl(notification_data)}">${notification_data.channel_name}</a> в Discord:`;
 
         notification_data.members.forEach((member) => {
-            text += `\n${member.user_name}\t\
-${member.muted && '🔇' || ' '}\
-${member.deafened && '🔕' || ' '}\
-${member.streaming && '🎥' || ' '}`;
+            text += `\n${member.member_name || member.user_name} <code>\
+${member.muted && '🔇' || '&#160;&#160;'}\
+${member.deafened && '🔕' || '&#160;&#160;'}\
+${member.streaming && '🖥️' || '&#160;&#160;'}\
+${member.camera && '🎥' || '&#160;&#160;'}</code>`;
         });
 
         return text;
