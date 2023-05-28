180691e1b3a15f5a16dd8701e09aa7df0694985c
Danila Vasilev <33842017+d0ctr@users.noreply.github.com>
2023-05-28 23:31:41 +0200

    Fix grammar

--- telegram/gpt-handler.js
+++ telegram/gpt-handler.js
@@ -181,7 +181,7 @@ class ChatGPTHandler{
             }).catch(err => {
                 this.logger.error('Error while sending ChatGPT completion', { error: err.stack || err });
                 interaction._reply(
-                    'Ты даже представить себе не можешь, что там ChatGPT придумал, давай поновой',
+                    'Ты даже представить себе не можешь, что там ChatGPT придумал, давай по новой',
                     { reply_to_message_id: prev_message_id }
                 ).catch(err => {
                     this.logger.error('Safe reply failed', { error: err.stack || err });
