ad3a2bdc7f2c93509649907c430dbd15b193735a
Danila Vasilev <33842017+d0ctr@users.noreply.github.com>
2024-01-30 14:09:55 +0000

    Update jsdoc, pages action

--- jsdoc.conf.js
+++ jsdoc.conf.js
@@ -1,3 +1,37 @@
+module.exports = {
+    "source": {
+        "include": [
+            "./api", 
+            "./commands",
+            "./discord",
+            "./services",
+            "./telegram"
+        ]
+    },
+    "sourceType": "script",
+    "plugins": [
+        "plugins/markdown"
+    ],
+    "opts": {
+        "recursive": true,
+        "destination": "./jsdoc/",
+        "template": "node_modules/docdash"
+    },
+    "docdash": {
+        "sectionOrder": [
+            "Modules",
+            "Namespaces",
+            "Interfaces",
+            "Classes",
+            "Externals",
+            "Events",
+            "Mixins",
+            "Tutorials",
+        ],
+        "typedefs": true
+    }
+};
+
 BigInt.prototype.toJSON = function() {
     return this.toString() + 'n';
 };
