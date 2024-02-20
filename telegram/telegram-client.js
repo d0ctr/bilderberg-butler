95844b3db3393dc251c3c47a0fc9794f4b455816
Danila Vasilev <mr.den.vi@gmail.com>
2024-02-20 11:42:37 +0000

    Use 'sendChatAction' as a webhook reply

--- telegram/telegram-client.js
+++ telegram/telegram-client.js
@@ -857,6 +857,7 @@ class TelegramClient {
 
         this.client = new Bot(process.env.TELEGRAM_TOKEN, {
             client: {
+                canUseWebhookReply: (method) => method === 'sendChatAction',
                 buildUrl: (root, token, method) => `${root}/bot${token}${process.env?.ENV === 'dev' ? '/test' : ''}/${method}`
             }
         });
