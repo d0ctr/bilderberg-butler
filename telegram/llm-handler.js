1973ee652e9424a18cea77d9b14ddb11a81e6ba3
Danila Vasilev <mr.den.vi@gmail.com>
2024-06-22 18:33:13 +0200

    Update llm-handler.js

--- telegram/llm-handler.js
+++ telegram/llm-handler.js
@@ -89,13 +89,13 @@ const { ADMIN_CHAT_ID } = require('../config.json');
  */
 const models = [
     'gpt-4o',
-    'claude-3-sonnet-20240229',
+    'claude-3-5-sonnet-20240620',
     'claude-3-opus-20240229'
 ];
 
 const max_tokens = {
     'gpt-4o': 4096,
-    'claude-3-sonnet-20240229': 4096,
+    'claude-3-5-sonnet-20240620': 4096,
     'claude-3-opus-20240229': 4096,
 }
 
@@ -115,7 +115,7 @@ const providers = [
 */
 const CHAT_MODEL_NAME = models.includes(process.env.LLM_MODEL) 
                         ? process.env.LLM_MODEL 
-                        : 'gpt-4o';
+                        : 'claude-3-5-sonnet-20240620';
 
 const DEFAULT_SYSTEM_PROMPT = `you are a chat-assistant embedded into a Telegram bot`;
 
